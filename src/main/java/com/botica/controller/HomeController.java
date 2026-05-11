2aac3fd6ec590b3a6a28211d863e8a109232ff2b
edwin <eespinozacorzo@gmail.com>
2026-05-10 23:11:45 -0500

    endpoint crear admin

--- src/main/java/com/botica/controller/HomeController.java
+++ src/main/java/com/botica/controller/HomeController.java
@@ -1,13 +1,33 @@
 package com.botica.controller;
 
+import com.botica.model.Usuario;
+import com.botica.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private UsuarioService usuarioService;
+
     @GetMapping("/")
     public String inicio() {
         return "index";
+    }
+
+    @GetMapping("/crear-admin")
+    @ResponseBody
+    public String crearAdmin() {
+        Usuario u = new Usuario();
+        u.setNombre("Administrador");
+        u.setUsername("admin2");
+        u.setPassword("admin123");
+        u.setRol("ADMIN");
+        u.setActivo(true);
+        usuarioService.guardar(u);
+        return "Admin creado exitosamente!";
     }
 }
