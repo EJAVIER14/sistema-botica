c158ca35bedbf0aafa2b1d4589b879cf40589e8a
edwin <eespinozacorzo@gmail.com>
2026-05-12 19:01:35 -0500

    eliminar endpoint setup por seguridad

--- src/main/java/com/botica/controller/HomeController.java
+++ src/main/java/com/botica/controller/HomeController.java
@@ -1,43 +1,13 @@
 package com.botica.controller;
 
-import com.botica.model.Usuario;
-import com.botica.repository.UsuarioRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-    @Autowired
-    private UsuarioRepository usuarioRepository;
-
     @GetMapping("/")
     public String inicio() {
         return "index";
-    }
-
-    @GetMapping("/setup")
-    @ResponseBody
-    public String setup() {
-        usuarioRepository.deleteAll();
-
-        BCryptPasswordEncoder encoder =
-                new BCryptPasswordEncoder();
-        String password = encoder.encode("admin123");
-
-        Usuario u = new Usuario();
-        u.setNombre("Administrador");
-        u.setUsername("admin");
-        u.setPassword(password);
-        u.setRol("ADMIN");
-        u.setActivo(true);
-
-        usuarioRepository.save(u);
-
-        return "Usuario creado! Password length: " +
-                password.length() + " - Pass: " + password;
     }
 }
